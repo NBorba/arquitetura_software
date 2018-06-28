958ecdbad34cd94bfc5435049be245e1baf98319
Renan B <renanblucas@gmail.com>
2018-06-27 21:09:45 -0300

    Adicionado projeto deploy

--- dogaoondemandweb/src/app/produto/httpproduto.service.ts
+++ dogaoondemandweb/src/app/produto/httpproduto.service.ts
@@ -12,7 +12,7 @@ export class HttpProdutoService {
 
   getProdutos(): Observable<Produto[]> {
     return this._http.
-      get('https://node12910-env-4599970.br1.saphir.global/rest/produto').
+      get('https://node3110-env-1654973.cloud.unispace.io/rest/produto').
       map(this.extractData);
   }
 
@@ -25,7 +25,7 @@ export class HttpProdutoService {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const options = new RequestOptions({ headers: headers });
     return this._http.
-      post('https://node12910-env-4599970.br1.saphir.global/rest/produto',
+      post('https://node3110-env-1654973.cloud.unispace.io/rest/produto',
       json, options).map(res => res.json());
   }
   
@@ -34,7 +34,7 @@ export class HttpProdutoService {
 	    const headers = new Headers({ 'Content-Type': 'application/json' });
 	    const options = new RequestOptions({ headers: headers });
 	    return this._http.
-	      post('https://node12910-env-4599970.br1.saphir.global/rest/produto',
+	      post('https://node3110-env-1654973.cloud.unispace.io/rest/produto',
 	      json, options).map(res => res.json());
 	  }
     
@@ -43,7 +43,7 @@ export class HttpProdutoService {
       const headers = new Headers({ 'Content-Type': 'application/json' });
       const options = new RequestOptions({ headers: headers });
       return this._http.
-        post('https://node12910-env-4599970.br1.saphir.global/rest/produto/excluir',
+        post('https://node3110-env-1654973.cloud.unispace.io/rest/produto/excluir',
         json, options).map(res => res.json());
     }
 }
